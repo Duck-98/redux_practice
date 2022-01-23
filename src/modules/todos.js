a8c635e2694e1051519e01d0d02933c19633b754
duck <godqhr2256@gmail.com>
2022-01-23 21:26:20 +0900

    초기 상태 및 리듀서 함수 생성

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -8,7 +8,7 @@ export const changeInput = (input) => ({
   input,
 });
 
-let id = 3;
+let id = 3; // insert가 호출되면 id 값이 1씩 상승
 export const insert = (text) => ({
   type: INSERT,
   todo: {
@@ -27,3 +27,51 @@ export const remove = (id) => ({
   type: REMOVE,
   id,
 });
+
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: '리덕스 기초 배우기',
+      done: true,
+    },
+    {
+      id: 2,
+      text: '리액트와 리덕스 사용하기',
+      done: false,
+    },
+  ],
+};
+
+function todos(state = initialState, action) {
+  switch (action.type) {
+    case CHANGE_INPUT:
+      return {
+        ...state,
+        input: action.input,
+      };
+
+    case INSERT:
+      return {
+        ...state,
+        todos: state.todos.concat(action.todo),
+      };
+    case TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+        ),
+      };
+    case REMOVE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.id),
+      };
+    default:
+      return state;
+  }
+}
+
+export default todos;
